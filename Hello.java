ca13520c108c87dd874bbe2fdd5b8fc35805c4c7
shrestha.sah@successive.tech <shrestha.sah@successive.tech>
2023-11-29 23:21:39 +0530

    first commit for java jenkins

---
+++ Hello.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Hello{
+    public static void main(String[] args) {
+        System.out.println("First project for jenkins");
+        System.out.println(new Date());
+    }
+}
