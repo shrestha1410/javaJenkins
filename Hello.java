6adb3ff6dd17560c79687f007bf39a730068b1e0
shrestha.sah@successive.tech <shrestha.sah@successive.tech>
2023-11-29 23:34:56 +0530

    added sysout

--- Hello.java
+++ Hello.java
@@ -4,5 +4,6 @@ public class Hello{
     public static void main(String[] args) {
         System.out.println("First project for jenkins");
         System.out.println(new Date());
+        System.out.println("Hello");
     }
 }
